4126761822c87f9e1af85f3f959d73e4af08e939
François-Xavier Guillois <40766799+tnntwister@users.noreply.github.com>
2022-03-22 17:44:24 +0100

    products test

--- givntek/models.py
+++ givntek/models.py
@@ -1,14 +1,31 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
+class Category(models.Model):
+    label = models.CharField(max_length=50)
+    slug = models.SlugField()
+    parent = models.IntegerField()
+    def __str__(self):
+        return "%s" % (self.label)
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    reference = models.CharField(max_length=60)
+    description = models.CharField(max_length=255)
+    price = models.FloatField()
+    categories =  models.ManyToManyField(Category)
+    exchange = models.BooleanField(blank=False)
+    donation = models.BooleanField(blank=False)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.title
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
