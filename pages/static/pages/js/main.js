cc48b5c312d9a0a00265370fcb28e654487c2cf5
François-Xavier Guillois <40766799+tnntwister@users.noreply.github.com>
2022-04-07 22:53:24 +0200

    menu

--- pages/static/pages/js/main.js
+++ pages/static/pages/js/main.js
@@ -1,1 +1,17 @@
-let app = new Reef('#app');
+// let app = new Reef('#app');
+
+var domReady = function (callback) {
+  document.readyState === "interactive" || document.readyState === "complete"
+    ? callback()
+    : document.addEventListener("DOMContentLoaded", callback);
+};
+
+domReady(function () {
+  document.querySelectorAll("a.mute").forEach(function (link) {
+    console.log(link);
+    link.addEventListener("click", function (event) {
+      console.log(document.querySelector("a.mute"));
+      event.preventDefault();
+    });
+  });
+});
